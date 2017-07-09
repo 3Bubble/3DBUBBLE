bda042ad6140355911043f74327c353f7ffb988a
Chris NK LI <nengkali2@gmail.com>
2017-07-09 21:19:07 +0800

    Add files via upload

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('F:\\WordPress Site\\httpd-2.4.18-x64-vc11\\Apache24\\htdocs\\wordpress\\wp-content\\plugins\\wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", 'F:\\WordPress Site\\httpd-2.4.18-x64-vc11\\Apache24\\htdocs\\wordpress/wp-content/wflogs/');
+	include_once 'F:\\WordPress Site\\httpd-2.4.18-x64-vc11\\Apache24\\htdocs\\wordpress\\wp-content\\plugins\\wordfence/waf/bootstrap.php';
+}
+?>
